8c6a0bf1b9b2c0a3d8d04cbd4b340a79e5bc97bb
Om Tambat <136994090+Onix284@users.noreply.github.com>
2025-06-12 22:40:38 +0530

    Signup Login API with bcrypt encryption
    - Signup and login API Completed.
    -Added bcrypt for password hashing.

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const db = require('./db');
+const db = require('../Server/config/db.js');
 const cors = require('cors');
-
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(express.json());
@@ -11,25 +11,96 @@ const PORT = process.env.PORT || 3000;
 
 app.post('/auth/signup', (req, res) => {
 
-    const { name, email, password, shopname } = req.body;
+    const { name, email, password, shop_name } = req.body;
 
-    if (!name || !email || !password || !shopname) {
+    if (!name || !email || !password || !shop_name) {
         return res.status(400).send('All fields are required');
     }
 
-    const query = 'INSERT INTO users (name, email, password, shopname ) VALUES (?, ?, ?, ?)';
-    
-    const values = [name, email, password, shopname];
+    // Step 1: Check if user already exists
+    const checkQuery = 'SELECT * FROM users WHERE email = ?';
 
-    db.query(query, values, (err, result) => {
+    db.query(checkQuery, [email], async (err, results) => {
+    if (err) return res.status(500).send('Database error');
+
+    if (results.length > 0) {
+      return res.status(409).json({ error: 'User already exists' });
+    }
+
+    // Step 2: Hash the password before storing
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+      // Step 3: Insert user into DB
+    const insertQuery = 'INSERT INTO users (name, email, password, shop_name) VALUES (?, ?, ?, ?)';
+
+    db.query(insertQuery, [name, email, hashedPassword, shop_name], (err, result) => {
+      if (err) {
+        console.error('Insert error:', err);
+        return res.status(500).send('Failed to register user');
+      }
+       res.status(201).json({ message: 'User registered successfully' });
+     });
+   });
+});
+
+
+app.post('/auth/login', (req, res) => {
+
+    const { email, password } = req.body;
+    
+    if(!email || !password){
+        return res.status(400).send('Email and password are required');
+    }
+
+    const query = 'SELECT * FROM users WHERE email = ?';
+
+    db.query(query, [email], async (err, results) => {
 
         if(err){
-            console.error('Error inserting user:', err);
+            console.error('Error fetching user:', err);
             return res.status(500).send('Internal server error');
         }
+
+        if(results.length === 0){
+            return res.status(401).json({ error: 'Invalid email' });
+        }
+
+        const user = results[0];
+
+        //Compare password with hashed password
+        const match = await bcrypt.compare(password, user.password);
+        if(!match){
+            return res.status(401).json({ error: 'Invalid password' });
+        }
+        
+
+        res.status(200).json({
+            message: 'Login successful',
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                shop_name: user.shop_name
+            }
+        });
+
     });
 
-    res.status(201).json({ message: 'User registered successfully' });
+});
+
+
+app.get('/users', (req, res) => {
+
+    const query = 'SELECT * FROM users';
+
+    db.query(query, (err, results) => {
+        
+        if(err){
+            return res.status(500).send('Internal server error');
+        }
+        res.json(results);
+    });
 });
 
 app.listen(PORT, () => {
@@ -37,3 +108,4 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 
 });
+
